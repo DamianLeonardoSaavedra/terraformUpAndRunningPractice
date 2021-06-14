a87a3d863e98e1eb899ca3de513900bd8abaf39d
Damian Saavedra <damianleosaavedra@gmail.com>
2021-06-14 08:02:19 -0300

    changes ami and security group

--- chapter2/main.tf
+++ chapter2/main.tf
@@ -1,7 +1,11 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
 resource "aws_instance" "enve-ec2-web-server" {
-  ami                    = "ami-0c55b159cbfafe1f0"
+  ami                    = "ami-09e67e426f25ce0d7"
   instance_type          = "t2.micro"
-  vpc_security_group_ids = [aws_security_group.instance.id]
+  vpc_security_group_ids = [aws_security_group.enve-ec2-web-server-sg.id]
 
 
   user_data = <<-EOF
